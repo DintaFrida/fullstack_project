c52c00c31c84cb7064fc42ef4710cb34e2d47a6d
Dinta Frida <dintafridayanti@gmail.com>
2026-04-09 22:30:29 +0700

    fix: remove node_modules

--- controllers/pembayaranController.js
+++ controllers/pembayaranController.js
@@ -1,11 +1,58 @@
-let pembayaran = [];
+const db = require("../backend/config/db.cjs");
 
+// CREATE
 exports.tambahPembayaran = (req, res) => {
-    const newPembayaran = { id: pembayaran.length + 1, ...req.body };
-    pembayaran.push(newPembayaran);
-    res.json({ message: "Pembayaran ditambahkan (mock)", pembayaranId: newPembayaran.id });
+  const data = req.body;
+
+  db.query("INSERT INTO pembayaran SET ?", data, (err, result) => {
+    if (err) return res.status(500).json(err);
+
+    res.json({
+      message: "Pembayaran berhasil ditambahkan",
+      id: result.insertId
+    });
+  });
 };
 
+// READ
 exports.getPembayaran = (req, res) => {
-    res.json(pembayaran);
+  db.query("SELECT * FROM pembayaran", (err, result) => {
+    if (err) return res.status(500).json(err);
+    res.json(result);
+  });
+};
+
+// 🔥 UPDATE
+exports.updatePembayaran = (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+
+  db.query(
+    "UPDATE pembayaran SET ? WHERE id_pembayaran = ?",
+    [data, id],
+    (err, result) => {
+      if (err) return res.status(500).json(err);
+
+      res.json({
+        message: "Pembayaran berhasil diupdate"
+      });
+    }
+  );
+};
+
+// 🔥 DELETE
+exports.deletePembayaran = (req, res) => {
+  const id = req.params.id;
+
+  db.query(
+    "DELETE FROM pembayaran WHERE id_pembayaran = ?",
+    [id],
+    (err, result) => {
+      if (err) return res.status(500).json(err);
+
+      res.json({
+        message: "Pembayaran berhasil dihapus"
+      });
+    }
+  );
 };
