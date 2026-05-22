42a714772c338bee9bb3c2341f81f0ac3fe79b01
Dinta Frida <dintafridayanti@gmail.com>
2026-05-22 13:51:07 +0700

    lapangan selesai

---
+++ src/components/LapanganList.jsx
@@ -0,0 +1,18 @@
+import LapanganCard from "./LapanganCard/LapanganCard"
+function LapanganList(){
+return(
+<>
+<LapanganCard
+nama="Lapangan A"
+jenis="Futsal"
+harga="Rp100.000"
+/>
+<LapanganCard
+nama="Lapangan B"
+jenis="Mini Soccer"
+harga="Rp150.000"
+/>
+</>
+)
+}
+export default LapanganList
